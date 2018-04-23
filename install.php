53f16a47b1e0fa8c48b25d26eb2c6772e1b6dcbc
Dirk Schürjohann <ds@decaf.de>
2018-04-23 13:51:31 +0200

    CS

--- install.php
+++ install.php
@@ -1,12 +1,16 @@
 <?php
+
 /** @var rex_addon $this */
+
 // copy backup files
 rex_dir::copy(
     $this->getPath('backups'),
     rex_addon::get('backup')->getDataPath()
 );
+
 // update config
 // merge current config with additional config
+
 // Background information:
 // We need the demo to be installed first of all to fetch required packages and import data.
 // To make this happen, we need to keep the config free of external dependencies and use an
@@ -15,4 +19,5 @@ $config = array_replace_recursive(
     rex_file::getConfig($this->getPath('package.yml')),
     rex_file::getConfig($this->getPath('package.setup.yml'))
 );
+
 rex_file::putConfig($this->getPath('package.yml'), $config);
